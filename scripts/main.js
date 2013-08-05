51eca181a4d2beaa9605b714c739aed587818126
clydeli <clyde1008li@gmail.com>
2013-08-05 13:48:15 -0700

    reverse to an even cleaner starting point

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ $(document).on('ready', function(){
   var generatedHTML = "";
 
   for(var i=0; i<=64; ++i){
-    generatedHTML += "<div class='cell'>"+i+"</div>";
+    generatedHTML += "<div>"+i+"</div>";
   }
 
   $('body').html(generatedHTML);
